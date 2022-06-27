457786c6f251404dc0ef0a46aa3f7930e7fa4c78
Hisham Unniyankal <uaj.hisham@gmail.com>
2022-06-27 03:50:55 +0200

    Added zoom to map, Code rearrangement, Error fixing, and Finalized geo map

--- js/mapChart.js
+++ js/mapChart.js
@@ -57,10 +57,11 @@ class MapChart {
             .attr("height", vis.HEIGHT)
             .attr("fill", "#eee")
             .on("click", function (d) {
+                vis.clicked(d)
                 clearCountryFilters(d)
-                vis.clicked()
             })
-
+        //Add g to svg
+        vis.g = vis.svg.append('g').attr('class', 'map_svg');
 
         //Create tooltip
         vis.tooltip = d3.select("body")
@@ -98,8 +99,15 @@ class MapChart {
         vis.legend_x = d3.scaleLinear()
             .domain([2.6, 75.1])
             .rangeRound([600, 860]);
-
-        vis.wrangleData()
+            
+        vis.zoom = d3.zoom()
+            .scaleExtent([1, 8])
+            .on('zoom', function() {
+            d3.selectAll('.country')
+            .attr('transform', d3.event.transform);
+      });
+      vis.svg.call(vis.zoom)
+      vis.wrangleData()
     }
 
     wrangleData() {
@@ -126,24 +134,17 @@ class MapChart {
         const vis = this
 
         vis.colorScale.domain(vis.ranges[vis.variable])
-            .range(d3.schemeReds[7]);
-        //Add g to svg
-        vis.g = vis.svg.append('g')
-            .attr('class', 'map_svg');
+            .range(["#eff3ff","#c6dbef","#9ecae1","#6baed6","#4292c6","#2171b5","#084594"]);
 
-        vis.g.selectAll("path").remove()
-        vis.map_selected = vis.g.selectAll("path")
-            .data(vis.geoMap.features)
-            .enter().append("path")
+        vis.mapplot = vis.g.selectAll(".country").data(vis.geoMap.features)
+
+        vis.mapplot.exit().remove()
             .attr("d", vis.projection)
             .attr("class", "country")
-            .attr("data-name", function (d) {
-                return d.properties.name
-            })
-            .attr("fill", function (d) {
-                d.total = vis.mapValueData[d.id] || 0;
+            .attr("data-name", function (d) { return d.properties.name })
+            .attr("fill", function (d) { d.total = vis.mapValueData[d.id] || 0;
                 if (d.total == 0) {
-                    return "#ddd"
+                    return "#FFF"
                 }
                 return vis.colorScale(d.total);
             })
@@ -152,7 +153,6 @@ class MapChart {
             .attr("id", function (d) {
                 return d.id
             })
-        vis.map_events = vis.map_selected
             .on("mouseover", function (d) {
                 d3.selectAll(".country")
                     .transition()
@@ -170,7 +170,69 @@ class MapChart {
                     .style("opacity", .9);
                 vis.tooltip.html(
                     "<h3> Country : " + d.properties.name + "</h3>"
-                    + "<p>" + vis.headings[vis.variable] + " : " + d.total + "</p>"
+                    + "<p>" + vis.headings[vis.variable] + " : " + customTickFormat(d.total) + "</p>"
+                )
+                vis.tooltip.style("left", (d3.event.pageX + 10) + "px")
+                    .style("top", (d3.event.pageY - 20) + "px")
+            })
+            .on("mouseleave", function (d) {
+                d3.selectAll(".country")
+                    .transition()
+                    .duration(100)
+                    .style("opacity", 1)
+                    .style("stroke-width", "0.5");
+                vis.tooltip.transition().duration(200)
+                    .style("opacity", 0);
+            })
+            .on("click", function (d) {
+                clearCountryFilters()
+                vis.filter_country_list = []
+                for (const key in vis.mapValueData) {
+                    if (key != d.id) {
+                        vis.filter_country_list.push(key)
+                    }
+                }
+                filterByCountry(vis.filter_country_list)
+            });
+        
+
+        vis.mapplot.enter().append("path")
+            .merge(vis.mapplot)
+            .attr("d", vis.projection)
+            .attr("class", "country")
+            .attr("data-name", function (d) {
+                return d.properties.name
+            })
+            .attr("fill", function (d) {
+                d.total = vis.mapValueData[d.id] || 0;
+                if (d.total == 0) {
+                    return "#FFF"
+                }
+                return vis.colorScale(d.total);
+            })
+            .attr("stroke", "#333")
+            .attr("stroke-width", "0.5")
+            .attr("id", function (d) {
+                return d.id
+            })
+            .on("mouseover", function (d) {
+                d3.selectAll(".country")
+                    .transition()
+                    .duration(100)
+                    .style("stroke-width", "0.5");
+
+                d3.select(this)
+                    .transition()
+                    .duration(100)
+                    .style("opacity", 1)
+                    .style("stroke-width", "2");
+
+                vis.tooltip.transition()
+                    .duration(150)
+                    .style("opacity", .9);
+                vis.tooltip.html(
+                    "<h3> Country : " + d.properties.name + "</h3>"
+                    + "<p>" + vis.headings[vis.variable] + " : " + customTickFormat(d.total) + "</p>"
                 )
                 vis.tooltip.style("left", (d3.event.pageX + 10) + "px")
                     .style("top", (d3.event.pageY - 20) + "px")
@@ -195,6 +257,7 @@ class MapChart {
                 }
                 filterByCountry(vis.filter_country_list)
             });
+
         vis.addLegend()
     }
 
@@ -329,11 +392,31 @@ class MapChart {
 
         vis.legend_heading.transition(vis.t).text(vis.headings[vis.variable]);
     }
-    clicked(d) {
+    clicked(d){
         const vis = this
-        
-        
 
+        var x, y, k;
+
+  if (d && centered !== d) {
+    var centroid = vis.projection.centroid(d);
+    x = centroid[0];
+    y = centroid[1];
+    k = 4;
+    centered = d;
+  } else {
+    x = vis.WIDTH / 2;
+    y = vis.HEIGHT / 2;
+    k = 1;
+    centered = null;
+  }
+
+  vis.g.selectAll("path")
+      .classed("active", centered && function(d) { return d === centered; });
+
+  vis.g.transition(vis.t)
+      .attr("transform", "translate(" + vis.WIDTH / 2 + "," + vis.HEIGHT / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+      .style("stroke-width", 1.5 / k + "px");
     }
+ 
 }
 
